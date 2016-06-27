7767d9631f6dc4a6ecc1eeda7f81accdd08ceac5
刘文果 <liuwenguo@liuwenguodemacbook-pro.local>
2016-06-27 09:06:24 +0800

    Initial Commit

---
+++ SwiftUseOCDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftUseOCDemo
+//
+//  Created by 刘文果 on 16/6/27.
+//  Copyright © 2016年 Fingerfive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
